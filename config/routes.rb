a9bce05edc5ed52c46a4538f55e5991a7cf54c3a
DESKTOP-4J3791P\Orange <chen.zhang.62@hotmail.com>
2022-05-26 22:02:37 +1000

    devise navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/restricted'
+  get '/restricted', to: "pages#restricted", as: "restricted"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
   # Defines the root path route ("/")
